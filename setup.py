f91cbca5fbc456302a668174836d5759cc5f8e44
k0kumar <72912657+k0kumar@users.noreply.github.com>
2025-08-22 17:03:41 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -161,7 +161,7 @@ setup(
         # guaranteed to be installable by `pip install` on all platforms.
         # To tell if a package is pure-python, go to https://pypi.org/project/{name}/#files
         "Pillow>=7.1",  # or use pillow-simd for better performance
-        "matplotlib",  # TODO move it to optional after we add opencv visualization
+        #"matplotlib",  # TODO move it to optional after we add opencv visualization
         #"pycocotools>=2.0.2",  # corresponds to https://github.com/ppwwyyxx/cocoapi
         # Do not add opencv here. Just like pytorch, user should install
         # opencv themselves, preferrably by OS's package manager, or by
